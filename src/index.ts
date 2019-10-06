94c33e496e0590fc1c053add98e11d32b356f50d
kenan hancer <kenan.hancer@lloydsbanking.com>
2019-10-06 08:08:11 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {Loopback4SimpleApiApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {Loopback4SimpleApiApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new Loopback4SimpleApiApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
